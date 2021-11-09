430a9c913e2fd11de9b36b64623c052944a68bd2
franssslay <73293223+franssslay@users.noreply.github.com>
2021-11-09 11:36:50 -0800

    Update 337.House Robber III.DP.java

--- Java/337.House Robber III.DP.java
+++ Java/337.House Robber III.DP.java
@@ -1,3 +1,28 @@
+// implement hashMap to store all nodes' values
+class Solution {
+    // function of f represents that the node is selected, function of g represents that the node is unselected
+    Map<TreeNode, Integer> f = new HashMap<TreeNode, Integer>();
+    Map<TreeNode, Integer> g = new HashMap<TreeNode, Integer>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
+        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
+    }
+}
+// time complexity : 25.43% faster O(n) 
+// space complextity : 56.74% faster O(n)
+
+// implement scolling array to optimize the time complexity and space complexity
 class Solution {
     public int rob(TreeNode root) {
         int[] rootStatus = dfs(root);
